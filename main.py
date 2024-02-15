6188d23d2ddacc89b4e25d471f64897f6adb6f04
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 18:52:13 -0500

    calling the function to generate reference space in main.py script

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import inputs
 import os
 import evolve 
 import initial_conditions
+import basis
 
 # creating a generatedfiles directory to save generated files
 try: os.makedirs('generatedfiles')
@@ -11,6 +12,9 @@ except: os.system('rm -r generatedfiles/*')
     
 # creating mesh
 grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements,inputs.p_basis_order)
+
+# generationg reference space information
+basis.generate_reference_space(inputs.p_basis_order,inputs.out_x_points_per_element,inputs.n_gauss_poins)
 
 # write initial conditions in 
 initial_conditions.write_initial_conditions()
