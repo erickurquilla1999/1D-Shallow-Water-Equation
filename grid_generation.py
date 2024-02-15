62a73a3ecadf3ab6ecef7514dcd751daf7890b14
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 10:30:58 -0500

    include subnodes inside each element

--- grid_generation.py
+++ grid_generation.py
@@ -1,10 +1,10 @@
 import utilities
 import numpy as np
 
-def generate_1d_mesh(initial_coord, final_coord, num_elements):
+def generate_1d_mesh(initial_coord, final_coord, num_elements,nodes_per_elements):
     # Generate node coordinates
     nodes = np.linspace(initial_coord, final_coord, num_elements + 1)
-    
+
     # Generate element numbers
     elements = np.arange(1, num_elements + 1)
     
@@ -15,4 +15,10 @@ def generate_1d_mesh(initial_coord, final_coord, num_elements):
     # Compute element lengths
     element_lengths = np.diff(nodes)
 
-    utilities.save_data_to_hdf5([elements, left_node_coords, right_node_coords, element_lengths],['element_number','left_node_coords','right_node_coords','element_lengths'],'generatedfiles/grid.h5')
+    # Compute subnode coordinates inside each element
+    subnodes_coord = []
+    for i in range(num_elements):
+        subnodes = np.linspace(nodes[i], nodes[i + 1], nodes_per_elements)
+        subnodes_coord.append(subnodes)
+
+    utilities.save_data_to_hdf5([elements, left_node_coords, right_node_coords, element_lengths, subnodes_coord],['element_number','left_node_coords','right_node_coords','element_lengths','subnodes_coords'],'generatedfiles/grid.h5')
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import inputs
 import os
 
 # creating a generatedfiles directory to save generated files
-os.makedirs('generatedfiles')
-
+try: os.makedirs('generatedfiles')
+except: os.system('rm -r generatedfiles/*')
+    
 # creating mesh
-grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements)
+grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements,inputs.p_basis_order)
