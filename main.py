9c5b8740b34192676c955ad14557d6ce2ccd3954
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-14 20:12:08 -0500

    creating a file to store the generated files

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import grid_generation
 import inputs
+import os
+
+# creating a generatedfiles directory to save generated files
+os.makedirs('generatedfiles')
 
 # creating mesh
 grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements)
