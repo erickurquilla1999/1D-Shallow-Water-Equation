8c8b432321a235a5cd148d2121ffd01f7f9a05ab
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 10:53:55 -0500

    better grid generator

--- grid_generation.py
+++ grid_generation.py
@@ -3,22 +3,22 @@ import numpy as np
 
 def generate_1d_mesh(initial_coord, final_coord, num_elements,nodes_per_elements):
     # Generate node coordinates
-    nodes = np.linspace(initial_coord, final_coord, num_elements + 1)
+    elements_division = np.linspace(initial_coord, final_coord, num_elements + 1)
 
     # Generate element numbers
-    elements = np.arange(1, num_elements + 1)
+    elements = np.arange(num_elements)
     
     # Compute node coordinates on the left and right sides of each element
-    left_node_coords = nodes[:-1]
-    right_node_coords = nodes[1:]
+    left_node_coords = elements_division[:-1]
+    right_node_coords = elements_division[1:]
     
     # Compute element lengths
-    element_lengths = np.diff(nodes)
+    element_lengths = np.diff(elements_division)
 
     # Compute subnode coordinates inside each element
-    subnodes_coord = []
+    nodes_coord = []
     for i in range(num_elements):
-        subnodes = np.linspace(nodes[i], nodes[i + 1], nodes_per_elements)
-        subnodes_coord.append(subnodes)
+        nodes = np.linspace(elements_division[i], elements_division[i + 1], nodes_per_elements)
+        nodes_coord.append(nodes)
 
-    utilities.save_data_to_hdf5([elements, left_node_coords, right_node_coords, element_lengths, subnodes_coord],['element_number','left_node_coords','right_node_coords','element_lengths','subnodes_coords'],'generatedfiles/grid.h5')
+    utilities.save_data_to_hdf5([elements, left_node_coords, right_node_coords, element_lengths, nodes_coord],['element_number','left_node_coords','right_node_coords','element_lengths','nodes_coords'],'generatedfiles/grid.h5')
