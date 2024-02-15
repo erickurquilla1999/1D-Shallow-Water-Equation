b8b182d3ac032d86211ae9c9b3ee2ead02c19cb5
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 12:09:19 -0500

    including output data parameters

--- inputs.py
+++ inputs.py
@@ -5,11 +5,12 @@ x_initial = 0 # (m) initial domain coordinate
 x_final = 10 # (m) final domain coordinate
 
 # DG method
-N_elements= 5 # number of elements
+N_elements= 10 # number of elements
 p_basis_order = 3 # lagrange basis order
 
 # simulation time
 n_steps = 15 # number of time steps
 t_limit = 100 # (s) simulation time limit
 
-
+# output data setting
+out_number_x_points= 100 # number of point to store data in each time step
