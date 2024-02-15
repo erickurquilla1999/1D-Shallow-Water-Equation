019676731183254579c89d7e518ac19b9bdac2a3
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 15:53:11 -0500

    moving function to write initial condition to initial_conditions.py script

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import grid_generation
 import inputs
 import os
 import evolve 
+import initial_conditions
 
 # creating a generatedfiles directory to save generated files
 try: os.makedirs('generatedfiles')
@@ -12,6 +13,6 @@ except: os.system('rm -r generatedfiles/*')
 grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements,inputs.p_basis_order)
 
 # write initial conditions in 
-evolve.write_initial_conditions()
+initial_conditions.write_initial_conditions()
 
 # evolve the PDE
