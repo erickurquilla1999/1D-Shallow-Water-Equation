5a24904359ab9676edae5e2e75f6921d944145f7
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 15:30:15 -0500

    final version of write initial conditons function

--- evolve.py
+++ evolve.py
@@ -3,6 +3,7 @@ import initial_conditions
 import utilities
 import inputs
 import basis
+import os
 
 
 # writing initial condition file: step_0.h5
@@ -12,8 +13,6 @@ def write_initial_conditions():
     #reading mesh file
     element_number = utilities.load_data_from_hdf5('element_number', 'generatedfiles/grid.h5')
     nodes_coords = utilities.load_data_from_hdf5('nodes_coords', 'generatedfiles/grid.h5')
-
-    print(nodes_coords)
 
     h_height=[]
     u_velocity=[]
@@ -26,54 +25,46 @@ def write_initial_conditions():
             u_ele.append(initial_conditions.velocity_initial(x_node))
         h_height.append(h_ele)
         u_velocity.append(u_ele)
+    
+    left_node_coords = utilities.load_data_from_hdf5('left_node_coords', 'generatedfiles/grid.h5')
+    right_node_coords = utilities.load_data_from_hdf5('right_node_coords', 'generatedfiles/grid.h5')
 
-    x_out = np.linspace(inputs.x_initial, inputs.x_final, inputs.out_number_x_points + 1)
+    x_out=[]
     h_out=[]
     u_out=[]
 
-    for x in x_out:
-        h_loc = 0
-        for n in element_number:
-            h_loc += basis.lagrange_interpolation(nodes_coords[n], h_height[n], x)
+    for n in element_number:
+        
+        x_loc = np.linspace(left_node_coords[n], right_node_coords[n], inputs.out_x_points_per_element + 1)
+        x_out.append(x_loc)
+        h_loc=[]
+        u_loc=[]
+
+        for x in x_loc:
+            h_loc.append(basis.lagrange_interpolation(nodes_coords[n], h_height[n], x))
         h_out.append(h_loc)
-    
-    for x in x_out:
-        u_loc = 0
-        for n in element_number:
-            u_loc += basis.lagrange_interpolation(nodes_coords[n], u_velocity[n], x)
+        
+        for x in x_loc:
+            u_loc.append(basis.lagrange_interpolation(nodes_coords[n], u_velocity[n], x))
         u_out.append(u_loc)
-    
 
-    import matplotlib.pyplot as plt     
-    from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,AutoMinorLocator,LogLocator)
-    import matplotlib as mpl
-    import matplotlib.ticker
+    # creating a output directory to save data
+    try: os.makedirs('output')
+    except: os.system('rm -r output/*')
 
-    mpl.rcParams['font.size'] = 22
-    mpl.rcParams['font.family'] = 'serif'
-    mpl.rc('text', usetex=True)
-    mpl.rcParams['xtick.major.size'] = 7
-    mpl.rcParams['xtick.major.width'] = 2
-    mpl.rcParams['xtick.major.pad'] = 8
-    mpl.rcParams['xtick.minor.size'] = 4
-    mpl.rcParams['xtick.minor.width'] = 2
-    mpl.rcParams['ytick.major.size'] = 7
-    mpl.rcParams['ytick.major.width'] = 2
-    mpl.rcParams['ytick.minor.size'] = 4
-    mpl.rcParams['ytick.minor.width'] = 2
-    mpl.rcParams['axes.linewidth'] = 2
+    utilities.save_data_to_hdf5([element_number,nodes_coords,h_height,u_velocity,x_out,h_out,u_out], ['element_number','nodes_coord','h_height','u_velocity','x_out','h_out','u_out'], 'output/step_1.h5')
 
-    fig, ax = plt.subplots()
+    # import matplotlib.pyplot as plt     
+    # fig, ax = plt.subplots()
+    # for n in element_number:
+    #     ax.plot(x_out[n],h_out[n])
 
-    ax.plot(x_out,h_out)
+    # h_theory=[]    
+    # for x in np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements):
+    #     h_theory.append(initial_conditions.initial_height(x))
+    # ax.plot(np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements),h_theory,c='black',linestyle='dotted')
 
-    h_theory=[]
-    
-    for x in x_out:
-        h_theory.append(initial_conditions.initial_height(x))
+    # fig.savefig('t_step_0.pdf',bbox_inches='tight')
+    # plt.show()
+    # plt.clf()
 
-    ax.plot(x_out,h_theory)
-
-    fig.savefig('t_step_0.pdf',bbox_inches='tight')
-    plt.show()
-    plt.clf()
