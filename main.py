715232f842b556a5007115b465abe5d400ebead7
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 21:36:50 -0500

    using reference space

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import basis
 # creating a generatedfiles directory to save generated files
 try: os.makedirs('generatedfiles')
 except: os.system('rm -r generatedfiles/*')
-    
+
 # creating mesh
-grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements,inputs.p_basis_order)
+element_number, nodes_coord,  left_node_coords, right_node_coords, nodes_coord_ref_space = grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements,inputs.p_basis_order)
 
 # generationg reference space information
-basis.generate_reference_space(inputs.p_basis_order,inputs.out_x_points_per_element,inputs.n_gauss_poins)
+element_number,nodes_coord_ref_space,basis_values_at_ref_coords,ref_coords_to_save_data,basis_values_at_the_point_to_save_data,gauss_coords_in_element,basis_values_at_gauss_coords,gauss_weights_in_element = basis.generate_reference_space(inputs.N_elements,inputs.p_basis_order,inputs.out_x_points_per_element,inputs.n_gauss_poins,nodes_coord_ref_space)
 
 # write initial conditions in 
-initial_conditions.write_initial_conditions()
+initial_conditions.write_initial_conditions(element_number, nodes_coord, left_node_coords, right_node_coords,ref_coords_to_save_data,basis_values_at_the_point_to_save_data)
 
 # evolve the PDE
