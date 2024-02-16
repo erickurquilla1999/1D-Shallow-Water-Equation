540b0e5fe27da4f9459664ea574981b115395712
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 20:08:14 -0500

    including reference space in grid information and solving error in computing nodes inside each element

--- grid_generation.py
+++ grid_generation.py
@@ -1,24 +1,28 @@
 import utilities
 import numpy as np
 
-def generate_1d_mesh(initial_coord, final_coord, num_elements,nodes_per_elements):
-    # Generate node coordinates
+def generate_1d_mesh(initial_coord, final_coord, num_elements, nodes_per_elements):
+    # Generate elements coordinates
     elements_division = np.linspace(initial_coord, final_coord, num_elements + 1)
 
     # Generate element numbers
     elements = np.arange(num_elements)
     
-    # Compute node coordinates on the left and right sides of each element
+    # Compute coordinates on the left and right sides of each element
     left_node_coords = elements_division[:-1]
     right_node_coords = elements_division[1:]
     
     # Compute element lengths
     element_lengths = np.diff(elements_division)
 
-    # Compute subnode coordinates inside each element
+    # Compute nodes coordinates inside each element
     nodes_coord = []
-    for i in range(num_elements):
-        nodes = np.linspace(elements_division[i], elements_division[i + 1], nodes_per_elements)
-        nodes_coord.append(nodes)
+    nodes_coord_ref_space = []
 
-    utilities.save_data_to_hdf5([elements, left_node_coords, right_node_coords, element_lengths, nodes_coord],['element_number','left_node_coords','right_node_coords','element_lengths','nodes_coords'],'generatedfiles/grid.h5')
+    for i in range(num_elements):
+        nodes_coord.append(np.linspace(elements_division[i], elements_division[i + 1], nodes_per_elements + 1))
+        nodes_coord_ref_space.append(np.linspace(-1,1,nodes_per_elements + 1))
+
+    utilities.save_data_to_hdf5([elements, left_node_coords, right_node_coords, element_lengths, nodes_coord, nodes_coord_ref_space],['element_number','left_node_coords','right_node_coords','element_lengths','nodes_coords', 'nodes_coord_ref_space'],'generatedfiles/grid.h5')
+
+    return elements, nodes_coord, left_node_coords, right_node_coords, nodes_coord_ref_space
