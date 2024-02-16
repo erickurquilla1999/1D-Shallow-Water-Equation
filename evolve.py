8d8e61a750e6c5f5e3552ba933c3b3d5a15eed89
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-16 14:14:41 -0500

    creationg function to compute residual vector

--- evolve.py
+++ evolve.py
@@ -29,7 +29,74 @@ def compute_M_matrix_inverse(element_number,basis_values_at_gauss_coords,gauss_w
 
     return M_inverse
 
-def compute_time_derivates(f1, f2, element_number):
+def compute_residual_vector(element_number,u_1,u_2,f_1,f_2,basis_values_at_gauss_coords,basis_derivative_values_at_gauss_coords,left_node_coords, right_node_coords,gauss_weights_in_elements,basis_values_at_ref_coords):
+
+    R_f_1=[]
+    R_f_2=[]
+
+    for n in element_number:
+        #interpolationg f_1 to gauss cuadrature
+        f_1_gauss=np.dot(basis_values_at_gauss_coords[n],f_1[n])
+        f_2_gauss=np.dot(basis_values_at_gauss_coords[n],f_2[n])
+
+        dphi_dx=np.array(basis_derivative_values_at_gauss_coords[n])
+
+        # resicual vector 1 
+        R_1_f_1 = 0.5*(right_node_coords[n]-left_node_coords[n])*np.dot(dphi_dx.T,f_1_gauss)
+        R_1_f_2 = 0.5*(right_node_coords[n]-left_node_coords[n])*np.dot(dphi_dx.T,f_2_gauss)
+
+        basis_at_initial_ele_node = basis_values_at_ref_coords[n][0]
+        basis_at_final_ele_node = basis_values_at_ref_coords[n][len(basis_values_at_ref_coords)-1]
+
+         # computing Roe flux
+        roe_flux_1_left=0
+        roe_flux_1_right=0
+        roe_flux_2_left=0
+        roe_flux_2_right=0
+
+        if n==0:
+            
+            roe_flux_1_left=0
+            roe_flux_2_left=inputs.g*u_1[n][n]**2/2
+
+            Jacobian_right = -inputs.g * 0.5 * ( u_1[n][len(u_1[n])-1] + u_1[n+1][0]) + ( ( f_1[n][len(f_1[n])-1] + f_1[n+1][0] ) / ( u_1[n][len(u_1[n])-1] + u_1[n+1][0] ) ) **2
+            
+            roe_flux_1_right = 0.5 * ( ( f_1[n][len(f_1[n])-1] + f_1[n+1][0] ) ) - 0.5 * Jacobian_right * ( u_1[n][len(u_1[n])-1] + u_1[n+1][0] )
+            roe_flux_2_right = 0.5 * ( ( f_2[n][len(f_2[n])-1] + f_2[n+1][0] ) ) - 0.5 * Jacobian_right * ( u_2[n][len(u_2[n])-1] + u_2[n+1][0] )
+        
+        elif n==len(element_number)-1:
+        
+            Jacobian_left = -inputs.g * 0.5 * ( u_1[n-1][len(u_1[n])-1] + u_1[n][0]) + ( ( f_1[n-1][len(f_1[n])-1] + f_1[n][0] ) / ( u_1[n-1][len(u_1[n])-1] + u_1[n][0] ) ) **2
+            
+            roe_flux_1_left = 0.5 * ( ( f_1[n-1][len(f_1[n])-1] + f_1[n][0] ) ) - 0.5 * Jacobian_left * ( u_1[n-1][len(u_1[n])-1] + u_1[n][0] )
+            roe_flux_2_left = 0.5 * ( ( f_2[n-1][len(f_2[n])-1] + f_2[n][0] ) ) - 0.5 * Jacobian_left * ( u_2[n-1][len(u_2[n])-1] + u_2[n][0] )
+
+            roe_flux_1_right=0
+            roe_flux_2_right=inputs.g*u_1[n][len(f_1[n])-1]**2/2
+
+        else:
+
+            Jacobian_left = -inputs.g * 0.5 * ( u_1[n-1][len(u_1[n])-1] + u_1[n][0]) + ( ( f_1[n-1][len(f_1[n])-1] + f_1[n][0] ) / ( u_1[n-1][len(u_1[n])-1] + u_1[n][0] ) ) **2
+            
+            roe_flux_1_left = 0.5 * ( ( f_1[n-1][len(f_1[n])-1] + f_1[n][0] ) ) - 0.5 * Jacobian_left * ( u_1[n-1][len(u_1[n])-1] + u_1[n][0] )
+            roe_flux_2_left = 0.5 * ( ( f_2[n-1][len(f_2[n])-1] + f_2[n][0] ) ) - 0.5 * Jacobian_left * ( u_2[n-1][len(u_2[n])-1] + u_2[n][0] )
+            
+            Jacobian_right = -inputs.g * 0.5 * ( u_1[n][len(u_1[n])-1] + u_1[n+1][0]) + ( ( f_1[n][len(f_1[n])-1] + f_1[n+1][0] ) / ( u_1[n][len(u_1[n])-1] + u_1[n+1][0] ) ) **2
+            
+            roe_flux_1_right = 0.5 * ( ( f_1[n][len(f_1[n])-1] + f_1[n+1][0] ) ) - 0.5 * Jacobian_right * ( u_1[n][len(u_1[n])-1] + u_1[n+1][0] )
+            roe_flux_2_right = 0.5 * ( ( f_2[n][len(f_2[n])-1] + f_2[n+1][0] ) ) - 0.5 * Jacobian_right * ( u_2[n][len(u_2[n])-1] + u_2[n+1][0] )
+
+        # resicual vector 2
+        R_2_f_1 = -(np.array(basis_at_final_ele_node) * np.array(roe_flux_1_right) - np.array(basis_at_initial_ele_node) * np.array(roe_flux_1_left))
+        R_2_f_2 = -(np.array(basis_at_final_ele_node) * np.array(roe_flux_2_right) - np.array(basis_at_initial_ele_node) * np.array(roe_flux_2_right))
+
+        # adding residual vector 1 and 2
+        R_f_1.append(R_1_f_1+R_2_f_1)
+        R_f_2.append(R_1_f_2+R_2_f_2)
+    
+    return R_f_1, R_f_2
+
+def compute_time_derivates(element_number,M_inverse, R_f_1, R_f_2 ):
     #Lopp over all element
     for n in element_number:
         a=0
--- main.py
+++ main.py
@@ -15,11 +15,24 @@ element_number, nodes_coord,  left_node_coords, right_node_coords, nodes_coord_r
 # generationg reference space information
 basis_values_at_ref_coords,ref_coords_to_save_data,basis_values_at_the_point_to_save_data,gauss_coords_in_elements,basis_values_at_gauss_coords,gauss_weights_in_elements,basis_derivative_values_at_gauss_coords = basis.generate_reference_space(inputs.N_elements,inputs.p_basis_order,inputs.out_x_points_per_element,inputs.n_gauss_poins,nodes_coord_ref_space)
 
-# write initial conditions in output/step_0.h5 directory
+# write initial conditions in output/step_0.h5 directory. f1=h and f2=hu
 f1, f2 = initial_conditions.write_initial_conditions(element_number, nodes_coord, left_node_coords, right_node_coords,ref_coords_to_save_data,basis_values_at_the_point_to_save_data)
 
 # compute matrix M and return the inverse matrix of M
 M_inverse = evolve.compute_M_matrix_inverse(element_number,basis_values_at_gauss_coords,gauss_weights_in_elements,left_node_coords, right_node_coords)
 
-# compute time derivatives of f1 and f2
-evolve.compute_time_derivates(f1, f2, element_number)
+#mapping shallow-water equations to eq par_t u_i + par_x f_i = 0. u=(h,hu) and f=(hu,hu^2+gh^2/2). f1=h and f2=hu.
+# setting the initil conditions to u and f components, u_i and f_i means u and f in component i 
+u_1 = f1
+u_2 = f2
+f_1 = f2
+f_2 = np.where(f1 == 0, 0, np.array(f2)**2 / f1 + inputs.g * np.array(f1)**2 / 2)
+
+# evolving in time the PDE
+for number_of_t_step in np.arange(inputs.n_steps):
+
+    #computing residual vector
+    R_f_1, R_f_2 = evolve.compute_residual_vector(element_number,u_1,u_2,f_1,f_2,basis_values_at_gauss_coords,basis_derivative_values_at_gauss_coords,left_node_coords, right_node_coords,gauss_weights_in_elements,basis_values_at_ref_coords)
+
+    # compute time derivatives of u_1 and u_2
+    du1_dt, du2_dt = evolve.compute_time_derivates(element_number,M_inverse, R_f_1, R_f_2)
