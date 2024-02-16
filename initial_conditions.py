4b151f350c00167f8991c1dfc509630e02cbff62
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-15 21:35:52 -0500

    using reference space

--- initial_conditions.py
+++ initial_conditions.py
@@ -19,12 +19,9 @@ def velocity_initial(x):
     
 # writing initial condition file: step_0.h5
 
-def write_initial_conditions():
+def write_initial_conditions(element_number,nodes_coords,left_node_coords,right_node_coords,ref_coords_to_save_data,basis_values_at_the_point_to_save_data):
 
-    #reading mesh file
-    element_number = utilities.load_data_from_hdf5('element_number', 'generatedfiles/grid.h5')
-    nodes_coords = utilities.load_data_from_hdf5('nodes_coords', 'generatedfiles/grid.h5')
-    
+    # setting the initial height and velocity in each element node
     h_height=[]
     u_velocity=[]
 
@@ -37,9 +34,7 @@ def write_initial_conditions():
         h_height.append(h_ele)
         u_velocity.append(u_ele)
     
-    left_node_coords = utilities.load_data_from_hdf5('left_node_coords', 'generatedfiles/grid.h5')
-    right_node_coords = utilities.load_data_from_hdf5('right_node_coords', 'generatedfiles/grid.h5')
-
+    # inpolating to save the output data
     x_out=[]
     h_out=[]
     u_out=[]
@@ -48,15 +43,15 @@ def write_initial_conditions():
         
         x_loc = np.linspace(left_node_coords[n], right_node_coords[n], inputs.out_x_points_per_element + 1)
         x_out.append(x_loc)
-        h_loc=[]
-        u_loc=[]
 
-        for x in x_loc:
-            h_loc.append(basis.lagrange_interpolation(nodes_coords[n], h_height[n], x))
+        h_loc=[]
+        for e in range(inputs.out_x_points_per_element+1):
+            h_loc.append(np.dot(basis_values_at_the_point_to_save_data[n][e], h_height[n]))
         h_out.append(h_loc)
-        
-        for x in x_loc:
-            u_loc.append(basis.lagrange_interpolation(nodes_coords[n], u_velocity[n], x))
+
+        u_loc=[]
+        for e in range(inputs.out_x_points_per_element+1):
+            u_loc.append(np.dot(basis_values_at_the_point_to_save_data[n][e], u_velocity[n]))
         u_out.append(u_loc)
 
     # creating a output directory to save data
@@ -65,7 +60,7 @@ def write_initial_conditions():
 
     utilities.save_data_to_hdf5([element_number,nodes_coords,h_height,u_velocity,x_out,h_out,u_out], ['element_number','nodes_coord','h_height','u_velocity','x_out','h_out','u_out'], 'output/step_1.h5')
 
-    # test
+    # testing the initial conditions scripts / see output/h_initial_conditions_test.pdf and see output/u_initial_conditions_test.pdf
 
     import matplotlib.pyplot as plt  
 
@@ -75,7 +70,10 @@ def write_initial_conditions():
     h_ini=[]    
     for x in np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements):
         h_ini.append(initial_conditions.initial_height(x))
-    ax.plot(np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements),h_ini,c='black',linestyle='dotted')
+    ax.plot(np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements),h_ini,c='black',linestyle='dotted',label='Real equation')
+    ax.set_xlabel(r'$x$ (m)')
+    ax.set_ylabel(r"$h$ (m)")
+    ax.legend()
     fig.savefig('output/h_initial_conditions_test.pdf',bbox_inches='tight')
     plt.clf()
 
@@ -85,6 +83,9 @@ def write_initial_conditions():
     u_ini=[]
     for x in np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements):
         u_ini.append(initial_conditions.velocity_initial(x))
-    ax.plot(np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements),u_ini,c='black',linestyle='dotted')
+    ax.plot(np.linspace(inputs.x_initial,inputs.x_final,inputs.out_x_points_per_element*inputs.N_elements),u_ini,c='black',linestyle='dotted',label='Real equation')
+    ax.set_xlabel(r'$x$ (m)')
+    ax.set_ylabel(r"$u$ (m)")
+    ax.legend()
     fig.savefig('output/u_initial_conditions_test.pdf',bbox_inches='tight')
     plt.clf()
