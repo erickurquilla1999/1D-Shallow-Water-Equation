7a77bd1596f01252e97303aa7e8c0a81ac20ed9d
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-17 11:00:55 -0500

    improving grid genertion script

--- grid_generation.py
+++ grid_generation.py
@@ -1,12 +1,12 @@
 import utilities
 import numpy as np
 
-def generate_1d_mesh(initial_coord, final_coord, num_elements, nodes_per_elements):
+def generate_1d_mesh(initial_coord, final_coord, num_elements, basis_order):
     # Generate elements coordinates
     elements_division = np.linspace(initial_coord, final_coord, num_elements + 1)
 
     # Generate element numbers
-    elements = np.arange(num_elements)
+    elements_numb = np.arange(num_elements)
     
     # Compute coordinates on the left and right sides of each element
     left_node_coords = elements_division[:-1]
@@ -15,17 +15,15 @@ def generate_1d_mesh(initial_coord, final_coord, num_elements, nodes_per_element
     # Compute element lengths
     element_lengths = np.diff(elements_division)
 
-    # Compute nodes coordinates in reference space and physical space inside each element
-    nodes_coord = []
-    nodes_coord_ref_space = []
+    # Compute nodes physical space inside each element
+    nodes_coord_phys_space = [np.linspace(elements_division[i], elements_division[i + 1], basis_order + 1) for i in elements_numb]
 
-    for i in range(num_elements):
-        nodes_coord.append(np.linspace(elements_division[i], elements_division[i + 1], nodes_per_elements + 1))
-        nodes_coord_ref_space.append(np.linspace(-1,1,nodes_per_elements + 1))
+    # Compute nodes refrecne space inside each element
+    nodes_coord_ref_space = [np.linspace(-1, 1, basis_order + 1) for _ in elements_numb]
     
     # save mesh information in 'generatedfiles/grid.h5'
-    utilities.save_data_to_hdf5([elements, left_node_coords, right_node_coords, element_lengths, nodes_coord, nodes_coord_ref_space],
-                                ['element_number','left_node_coords','right_node_coords','element_lengths','nodes_coords', 'nodes_coord_ref_space'],
+    utilities.save_data_to_hdf5([elements_numb, left_node_coords, right_node_coords, element_lengths, nodes_coord_phys_space, nodes_coord_ref_space],
+                                ['element_number','left_node_coords','right_node_coords','element_lengths','nodes_coord_phys_space', 'nodes_coord_ref_space'],
                                 'generatedfiles/grid.h5')
 
-    return elements, nodes_coord, left_node_coords, right_node_coords, nodes_coord_ref_space
+    return elements_numb, left_node_coords, right_node_coords, nodes_coord_phys_space, nodes_coord_ref_space
