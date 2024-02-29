c448438522797adb1deba37fd21c20e49342c865
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-28 20:46:59 -0500

    setting some automatic test on several functions

--- inputs.py
+++ inputs.py
@@ -5,21 +5,21 @@ x_initial = 0 # (m) initial domain coordinate
 x_final = 10 # (m) final domain coordinate
 
 # DG method
-N_elements= 30 # number of elements
-p_basis_order = 5 # lagrange basis order
+N_elements= 16 # number of elements
+p_basis_order = 2 # lagrange basis order
 
 # simulation time
-n_steps = 4000 # number of time steps
+n_steps = 12000 # number of time steps
 t_step = 0.00001 # (s) simulation time limit
 
 # Gauss cuadrature
-n_gauss_poins = 40 
+n_gauss_poins = 20 
 
 # constant
 g = 9.8 # m/s^2
 
 # plotting setting
-plot_every_steps = 200
+plot_every_steps = 2000
 
 # PDE evolution method # 0: euler, 1: rk4
-evolution_method = 1
+evolution_method = 0
