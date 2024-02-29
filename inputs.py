a6604157e94b12844e3e24ea41d6ed97004929fc
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-29 13:20:40 -0500

    initial input parameters

--- inputs.py
+++ inputs.py
@@ -5,12 +5,12 @@ x_initial = 0 # (m) initial domain coordinate
 x_final = 10 # (m) final domain coordinate
 
 # DG method
-N_elements= 16 # number of elements
+N_elements= 10 # number of elements
 p_basis_order = 2 # lagrange basis order
 
 # simulation time
-n_steps = 12000 # number of time steps
-t_step = 0.00001 # (s) simulation time limit
+n_steps = 100 # number of time steps
+t_step = 1 # (s) simulation time limit
 
 # Gauss cuadrature
 n_gauss_poins = 20 
@@ -19,7 +19,7 @@ n_gauss_poins = 20
 g = 9.8 # m/s^2
 
 # plotting setting
-plot_every_steps = 2000
+plot_every_steps = 5
 
 # PDE evolution method # 0: euler, 1: rk4
 evolution_method = 0
