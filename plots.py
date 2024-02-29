c65f49bb3a05bf616b54d2eb31dbc07c90fe9319
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-02-29 17:39:57 -0500

    now write the time in the plot

--- plots.py
+++ plots.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 import utilities
+import inputs
 
 def plotting():
 
@@ -30,7 +31,9 @@ def plotting():
     for file in files:
     
         print(f'Plotting {file}')
-         
+
+        step = int(file.split('_')[1].split('.')[0])
+
         x = utilities.load_data_from_hdf5('nodes_coordinates','output/'+file)
         velocity = utilities.load_data_from_hdf5('velocity','output/'+file)
         height = utilities.load_data_from_hdf5('height','output/'+file)
@@ -42,14 +45,16 @@ def plotting():
         ax.set_xlabel(r'$x$ (m)')
         ax.set_ylabel(r'$h$ (m)')
         ax.set_ylim(0.5,1.5)
+        ax.text(0.1, 1, "time = "+str(step*inputs.t_step)+" s", fontsize=12)
         fig.savefig('plots/h_'+file[0:-3]+'.pdf',bbox_inches='tight')
         plt.close(fig) 
 
         fig, ax = plt.subplots()
         for i in range(len(x)):
             ax.plot(x[i],velocity[i])
-            ax.scatter(x[i],velocity[i])
+            # ax.scatter(x[i],velocity[i])
         ax.set_xlabel(r'$x$ (m)')
         ax.set_ylabel(r'$u$ (m/s)')
+        ax.text(0.1, 0, "time = "+str(step*inputs.t_step)+" s", fontsize=12)
         fig.savefig('plots/u_'+file[0:-3]+'.pdf',bbox_inches='tight')
         plt.close(fig) 
